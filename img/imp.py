6dfd65135bfa4a35be2a1a04fbd7da3d8d1ae1f8
MinHyunSeong <sgustjd2@ff00ff.kr>
2020-09-08 15:57:42 +0900

    Optical flow & ArCuo test

--- img/imp.py
+++ img/imp.py
@@ -14,12 +14,6 @@ print("opencv:", cv2.__version__)
 device_bool = False
 fileCounter = 0
 
-#ROI setting
-x_roi = 100
-y_roi = 40
-w_roi = 440
-h_roi = 400
-
 realsense_check = os.system("lsusb -d8086:0b07")
 if(realsense_check==0):     #device 연결 됐을 경우
     device_bool = True
@@ -29,6 +23,7 @@ else:
     print("> NOT connect realsense ! \n> python3 system exit !!!")
     sys.exit()
 
+'''
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 500,
                        qualityLevel = 0.3,
@@ -39,7 +34,59 @@ feature_params = dict( maxCorners = 500,
 lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+'''
+#ROI setting
+x_roi = 100
+y_roi = 40
+w_roi = 440
+h_roi = 400
 
+#aruco
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+parameters = cv2.aruco.DetectorParameters_create()
+
+def aruco_detect(src_img):
+    gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+    # Detect Aruco markers
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray_img, aruco_dict, parameters=parameters)
+    #print("corners:",corners)
+    #print("ids:",ids)
+    frame_markers = cv2.aruco.drawDetectedMarkers(src_img.copy(), corners, ids)
+
+    '''
+    if np.all(ids != None):
+        frame_markers = cv2.aruco.drawDetectedMarkers(src_img.copy(), corners, ids)
+        x1 = (corners[0][0][0][0], corners[0][0][0][1])
+        x2 = (corners[0][0][1][0], corners[0][0][1][1])
+        x3 = (corners[0][0][2][0], corners[0][0][2][1])
+        x4 = (corners[0][0][3][0], corners[0][0][3][1])
+
+        im_dst = src_img
+        im_src = cv2.imread("test.jpeg")
+        size = im_dst.shape
+        pts_dst = np.array([x1, x2, x3, x4])
+        pts_src = np.array(
+            [
+                [0, 0],
+                [size[1] - 1, 0],
+                [size[1] - 1, size[0] - 1],
+                [0, size[0] - 1]
+            ], dtype=float
+        )
+        h, status = cv2.findHomography(pts_src, pts_dst)
+        temp = cv2.warpPerspective(
+            im_src, h, (im_dst.shape[1], im_dst.shape[0]))
+        cv2.fillConvexPoly(im_dst, pts_dst.astype(int), 0, 16)
+        im_dst = im_dst + temp
+        cv2.imshow('Display', im_dst)
+    else:
+        display = src_img
+        cv2.imshow('Display', display)
+    '''
+    cv2.namedWindow('frame_markers', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('frame_markers', frame_markers)
+
+    #return frame_markers
 
 def diff(src1, src2):
     if src1 is None:
@@ -87,32 +134,30 @@ def diff(src1, src2):
     cv2.imshow('img', img)
     p0 = good_new.reshape(-1, 1, 2)
     '''
-    '''
     #calcOpticalFlowFarneback
-    flow = cv2.calcOpticalFlowFarneback(
-        gray_src1, gray_src2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv2.calcOpticalFlowFarneback(gray_src1, gray_src2, None, 0.5, 3, 13, 3, 5, 1.1, 0)
+    #print(flow)
     img = draw_flow(gray_src2, flow)
     cv2.namedWindow('img', cv2.WINDOW_AUTOSIZE)
     cv2.imshow('img', img)
-    '''
     print(diff_value)
     return diff_value
-'''
+
 #calcOpticalFlowFarneback
-STEP = 16
-QUIVER = (255, 100, 0)
-def draw_flow(img, flow, step=STEP):
+def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
     y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(int)
     fx, fy = flow[y, x].T
-    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines + 0.5)
     vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.polylines(vis, lines, 0, QUIVER)
+    cv2.polylines(vis, lines, 0, (0, 255, 255), lineType=cv2.LINE_AA)
+
     for (x1, y1), (_x2, _y2) in lines:
-        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+        cv2.circle(vis, (x1, y1), 1, (0, 128, 255), -1, lineType=cv2.LINE_AA)
+
     return vis
-'''
+
 print ("> Configure depth and color streams")
 # Configure depth and color streams
 pipeline = rs.pipeline()    #카메라 구성 및 스트리밍, 비전 모듈 트리거링 미치 스레딩 추상화
@@ -154,6 +199,7 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
+        aruco_detect(color_image)
         diff_value = diff(first_frame, color_image)
         first_frame = color_image
 
@@ -164,7 +210,7 @@ try:
             filename = f'cimg%i.jpg' % fileCounter
             cv2.imwrite(filename, color_image)
             # depth_image 이미미지 저장
-            filename = f'dimg%i.jpg' % fileCounter
+            filename = f'dimg%i.jpg' q% fileCounter
             cv2.imwrite(filename, depth_image)
             # depth_colormap 이미지 저장
             # filename = f'd_c_img%i.jpg' % fileCounter
