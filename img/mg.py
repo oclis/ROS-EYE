9a8c2e74da6b1332644316cfc2dbb472cdb493e8
kwon <oclis@naver.com>
2020-09-15 20:09:45 +0900

    명령어가 입력되는 폼으로 QWidget으로 작성
    - MSSQL DB에 품종이름과 대표 이미지를 입력하는 기능 작성 예정
    - Dual Display 
    - Thread 분리

--- img/mg.py
+++ img/mg.py
@@ -1,105 +1,138 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox, QLineEdit,QPushButton, QTextEdit, QMainWindow
+from PyQt5.QtGui import QPixmap,QImage
 import socket
 import time
 import sys
 import cv2
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread, QRect
 import numpy as np
+import img_client
 
 HOST = '223.171.46.135'
 PORT = 9999
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
 
-fileCounter = 0
-
-
-class ClientThread(QThread):
-    change_pixmap_signal = pyqtSignal(np.ndarray)
-    clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #socket에서 수신한 버퍼를 반환하는 함수
-    def recvall(self, sock, count):
-        buf = b''
-        while count:
-            newbuf = sock.recv(count)
-            if not newbuf: return None
-            buf += newbuf
-            count -= len(newbuf)            
-        return buf
-
+class Ui_ImageDialog(QWidget):
     def __init__(self):
-        #Create socket and connect to the server
+        print('start')
         super().__init__()
-        print('Socket Created')
-        self.clnt_sock.connect(('223.171.46.135', 9999))
-        print('Socket Connected')
-        self._run_flag = True
+        print('__init__')
+        self.c = img_client.ClientSocket(self)
+        print('ClientSocket')
+        self.initUI()
 
-    def run(self):
-	    #Decode the data and generate an image file
-        while self._run_flag:
-            length = self.recvall( self.clnt_sock, 16)
-            StringData =  self.recvall(self.clnt_sock, int(length))
-            data = np.fromstring(StringData, dtype='uint8')
-            decode_img = cv2.imdecode(data, 1)
-            if int(length) > 0:
-                self.change_pixmap_signal.emit(decode_img)
+    def __del__(self):
+        self.c.stop()
 
+    def initUI(self):
+# 클라이언트 설정 부분
+        print('initUI') 
+        self.setWindowTitle('Image Client')
+        ipbox = QHBoxLayout()
 
-    def stop(self):
-        """Sets run flag to False and waits for thread to finish"""
-        self._run_flag = False
-        self.wait()
+        gb = QGroupBox('서버 설정')
+        ipbox.addWidget(gb)
+        box = QHBoxLayout()
+        label = QLabel('Server IP')
+        
+        self.ip = QLineEdit('223.171.46.135')
+        self.ip.setInputMask('000.000.000.000;_')
+        box.addWidget(label)
+        box.addWidget(self.ip)
+        label = QLabel('Server Port')
+        self.port = QLineEdit(str(PORT))
+        box.addWidget(label)
+        box.addWidget(self.port)
 
+        self.btn = QPushButton('접속')  
+        self.btn.clicked.connect(self.connectClicked)
+        box.addWidget(self.btn)
+        gb.setLayout(box)
+        # 채팅창 부분  
+        infobox = QHBoxLayout()
+        gb = QGroupBox('Image') 
+        infobox.addWidget(gb)
+        box = QVBoxLayout()
+        
+        label = QLabel('받은 메시지')
+        box.addWidget(label)
 
-class App(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Magenta live label")
-        self.disply_width = 640
-        self.display_height = 480
-        # create the label that holds the image
-        self.image_label = QLabel(self)
-        self.image_label.resize(self.disply_width, self.display_height)
-        # create a text label
-        self.textLabel = QLabel('Fusys Powder Coating line')
+        self.image_label = QLabel()
+        self.image_label.setGeometry(QRect(110, 0, 750, 480))
+        self.image_label.setText("")
+        self.image_label.setPixmap(QtGui.QPixmap("c.jpg"))
+        self.image_label.setAlignment(Qt.AlignCenter)
+        self.image_label.setWordWrap(False)
+        self.image_label.setObjectName("image_label")
+        box.addWidget(self.image_label)
 
-        # create a vertical box layout and add the two labels
+        label = QLabel('명령어')
+        box.addWidget(label)
+        self.sendmsg = QTextEdit()
+        self.sendmsg.setFixedHeight(50)
+        box.addWidget(self.sendmsg)
+        hbox = QHBoxLayout()
+        box.addLayout(hbox)
+        self.sendbtn = QPushButton('보내기')
+        self.sendbtn.setAutoDefault(True)
+        self.sendbtn.clicked.connect(self.sendMsg)
+        
+        self.clearbtn = QPushButton('명령 지움')
+        self.clearbtn.clicked.connect(self.clearMsg)
+        hbox.addWidget(self.sendbtn)
+        hbox.addWidget(self.clearbtn)
+        gb.setLayout(box)
+        
         vbox = QVBoxLayout()
-        vbox.addWidget(self.image_label)
-        vbox.addWidget(self.textLabel)
-        # set the vbox layout as the widgets layout
+        vbox.addLayout(ipbox)
+        vbox.addLayout(infobox)
         self.setLayout(vbox)
+        self.show()
+        print('initUI end') 
 
-        # create the video capture thread
-        self.thread = ClientThread()
-        # connect its signal to the update_image slot
-        self.thread.change_pixmap_signal.connect(self.update_image)
-        # start the thread
-        self.thread.start()
+    def connectClicked(self):
+          if self.c.bConnect == False:
+               ip = self.ip.text()
+               port = self.port.text()
+               if self.c.connectServer(ip, int(port)):
+                    self.btn.setText('접속 종료')
+               else:
+                    self.c.stop()
+                    self.sendmsg.clear()
+                    self.btn.setText('접속')
+          else:
+                self.c.stop()
+                self.sendmsg.clear()
+                self.recvmsg.clear()
+                self.btn.setText('접속')
+    def connectSignal(self):
+        self.c.recv.recv_signal.connect(self.update_image)
+        self.c.disconn.disconn_signal.connect(self.updateDisconnect)    
 
-    def closeEvent(self, event):
-        self.thread.stop()
-        event.accept()
+    def updateImageLable(self, q_image):        
+        self.image_label.setPixmap(QPixmap.fromImage(q_image))
 
+    def updateDisconnect(self):
+        self.btn.setText('접속')
 
-    @pyqtSlot(np.ndarray)
-    def update_image(self, cv_img):
-        """Updates the image_label with a new opencv image"""
-        qt_img = self.convert_cv_qt(cv_img)
-        self.image_label.setPixmap(qt_img)
-    
-    def convert_cv_qt(self, cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
-        return QPixmap.fromImage(p)
-    
-if __name__=="__main__":
+    def sendMsg(self):
+        sendmsg = self.sendmsg.toPlainText()
+        self.c.send(sendmsg)
+        self.sendmsg.clear()
+
+    def clearMsg(self):
+        pass
+
+    def closeEvent(self, e):
+        self.c.stop() 
+
+    @pyqtSlot(QImage)
+    def update_image(self,q_img):
+        self.updateImageLable(q_img)
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    a = App()
-    a.show()
+    ui = Ui_ImageDialog()
+    ui.connectSignal()
     sys.exit(app.exec_())
