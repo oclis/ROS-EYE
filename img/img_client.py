0a8ee11e1e4e23924418c4fbd5b0747b1fc0a4ff
MinHyunSeong <70618873+MinHyunSeong@users.noreply.github.com>
2020-09-22 16:25:26 +0900

    9월22일(화)
    1. 상품 유무 및 기계동작유무 데이터 받기 설정
    2. 유무 값에 따라 이미지저장

--- img/img_client.py
+++ img/img_client.py
@@ -4,6 +4,10 @@ from PyQt5.QtCore import Qt, pyqtSignal, QObject
 from PyQt5.QtGui import QImage
 import numpy as np
 import cv2
+import datetime
+import os
+img_date = datetime.datetime.now().strftime("%Y%m%d")
+PATH = '../../img_fursys/' + img_date
 
 class Signal(QObject):  
     recv_signal = pyqtSignal(np.ndarray)
@@ -55,26 +59,40 @@ class ClientSocket:
             newbuf = sock.recv(count)
             if not newbuf: return None
             buf += newbuf
-            count -= len(newbuf)            
+            count -= len(newbuf)
+
         return buf
 
     def receive(self, client):
         while self.bConnect:            
-            try:                                   
+            try:
+                states = self.recvall(self.client,3)
                 length = self.recvall( self.client, 16)
                 StringData =  self.recvall(self.client, int(length))
+                states = states.decode()
+                state_list = states.split('@')
+                action_state = state_list[0]
+                goods_state = state_list[1]
+                print("action state :" + action_state)
+                print("good state : " + goods_state)
                 data = np.fromstring(StringData, dtype='uint8')
                 decode_img = cv2.imdecode(data, 1)
-                    
+                img_date = datetime.datetime.now().strftime("%H_%M_%S")
+
                 self.fileCounter += 1
-                if self.fileCounter % 2 == 0: 
+                if self.fileCounter % 2 == 0:
                     if self.bMode:                  
-                        self.recv.recv_signal.emit(decode_img)  
+                        self.recv.recv_signal.emit(decode_img)
+                        filename = img_date + '_color.jpg'  # color 파일생성
+                        if action_state == 'A' and goods_state == 'Y' :
+                            cv2.imwrite(os.path.join(PATH,filename), decode_img)
                 else:
                     if not self.bMode:                   
-                        self.recv.recv_signal.emit(decode_img)    
-               
-                                              
+                        self.recv.recv_signal.emit(decode_img)
+                        filename = img_date + '_depth.jpg'  # depth 파일생성
+                        if action_state == 'A' and goods_state == 'Y':
+                            cv2.imwrite(os.path.join(PATH,filename), decode_img)
+
             except Exception as e:
                 print('Recv() Error :', e)                
                 break
