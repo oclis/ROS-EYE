c322ec5fafce502c0e01a4b822ce8d2d11a058e2
ksshin <sinsinsin01@gmail.com>
2020-09-25 13:58:04 +0900

    파일명-날짜+시간으로 저장

--- img/img_client.py
+++ img/img_client.py
@@ -6,34 +6,36 @@ import numpy as np
 import cv2
 import datetime
 import os
+
 img_date = datetime.datetime.now().strftime("%Y%m%d")
 PATH = '../../../img_fursys/' + img_date
 
-class Signal(QObject):  
+
+class Signal(QObject):
     recv_signal = pyqtSignal(np.ndarray)
     disconn_signal = pyqtSignal()
+
 
 class ClientSocket:
     fileCounter = 0
 
-    def __init__(self, parent):      
-        print('ClientSocket int')  
-        self.parent = parent                        
-        self.recv = Signal() 
-        self.disconn = Signal() 
+    def __init__(self, parent):
+        print('ClientSocket int')
+        self.parent = parent
+        self.recv = Signal()
+        self.disconn = Signal()
 
         self.bConnect = False
         self.bMode = False
 
-         
     def __del__(self):
         self.stop()
- 
+
     def connectServer(self, ip, port):
-        self.client = socket(AF_INET, SOCK_STREAM)           
- 
+        self.client = socket(AF_INET, SOCK_STREAM)
+
         try:
-            self.client.connect( (ip, port) )
+            self.client.connect((ip, port))
         except Exception as e:
             print('Connect Error : ', e)
             return False
@@ -42,15 +44,15 @@ class ClientSocket:
             self.t = Thread(target=self.receive, args=(self.client,))
             self.t.start()
             print('Connected')
- 
+
         return True
- 
+
     def stop(self):
-        self.bConnect = False       
-        if hasattr(self, 'client'):            
+        self.bConnect = False
+        if hasattr(self, 'client'):
             self.client.close()
-            del(self.client)
-            print('Client Stop') 
+            del (self.client)
+            print('Client Stop')
             self.disconn.disconn_signal.emit()
 
     def recvall(self, sock, count):
@@ -64,57 +66,56 @@ class ClientSocket:
         return buf
 
     def receive(self, client):
-        while self.bConnect:            
+        while self.bConnect:
             try:
-                states = self.recvall(self.client,3)
-                length = self.recvall( self.client, 16)
-                StringData =  self.recvall(self.client, int(length))
+                states = self.recvall(self.client, 3)
+                length = self.recvall(self.client, 16)
+                StringData = self.recvall(self.client, int(length))
                 states = states.decode()
                 state_list = states.split('@')
                 action_state = state_list[0]
                 goods_state = state_list[1]
-                #print("action state :" + action_state)
-                #print("good state : " + goods_state)
+                # print("action state :" + action_state)
+                # print("good state : " + goods_state)
                 data = np.fromstring(StringData, dtype='uint8')
                 decode_img = cv2.imdecode(data, 1)
-                img_date = datetime.datetime.now().strftime("%H_%M_%S")
+                img_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
                 self.fileCounter += 1
                 if self.fileCounter % 2 == 0:
-                    if self.bMode:                  
+                    if self.bMode:
                         self.recv.recv_signal.emit(decode_img)
-                        filename = img_date + '_depth.jpg'  # depth 파일생성
-                        if action_state == 'A' and goods_state == 'Y' :
-                            cv2.imwrite(os.path.join(PATH,filename), decode_img)
-                else:
-                    if not self.bMode:                   
-                        self.recv.recv_signal.emit(decode_img)
-                        filename = img_date + '_color.jpg'  # color 파일생성
+                        filename = img_time + '_depth.jpg'  # depth 파일생성
                         if action_state == 'A' and goods_state == 'Y':
-                            cv2.imwrite(os.path.join(PATH,filename), decode_img)
+                            cv2.imwrite(os.path.join(PATH, filename), decode_img)
+                else:
+                    if not self.bMode:
+                        self.recv.recv_signal.emit(decode_img)
+                        filename = img_time + '_color.jpg'  # color 파일생성
+                        if action_state == 'A' and goods_state == 'Y':
+                            cv2.imwrite(os.path.join(PATH, filename), decode_img)
 
             except Exception as e:
-                print('Recv() Error :', e)                
+                print('Recv() Error :', e)
                 break
- 
+
         self.stop()
 
     def changeMode(self):
 
-        if not self.bConnect :
+        if not self.bConnect:
             print('Has no connect!')
             return
         self.bMode = not self.bMode
-        if self.bMode :
+        if self.bMode:
             print('change mode == True')
         else:
             print('False')
 
-
     def send(self, msg):
         if not self.bConnect:
             return
-        try:            
+        try:
             self.client.send(msg.encode())
         except Exception as e:
-            print('send() Error :', e)   
+            print('send() Error :', e)
